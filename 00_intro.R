69c6c5b8225c15a867c26d37b458241957611c34
Tomás Barcellos <tomasbarcellos@gmail.com>
2019-07-01 02:29:21 -0300

    adiciona aula 1 e seus exercicios

---
+++ 00_intro.R
@@ -0,0 +1,77 @@
+# R base ----------------------------------------------------------------------
+# 1. Crie um objeto (nomes) que tenha o seu nome e o nome de um colega
+
+
+# 2. Crie um objeto (alturas) que tenha a sua altura e a altura do mesmo colega
+
+
+# 3. Selecione os nomes de quem tem mais de 1.80 metros de altura
+
+
+# Use o vetor valores, abaixo, para os próximos exercícios
+set.seed(123)
+valores <- rnorm(100, 1.75, 0.08)
+
+# 4. Quantos valores estão acima de 1.85?
+
+
+# 5. Crie um vetor "altos" com apenas valores maiores do que 1.85
+
+
+# 6. Selecione apenas os valores abaixo de 1.65 ou maiores do que 1.85
+
+
+# 7. Selecione apenas os valores entre 1.65 e 1.85
+
+
+# 8. Dê mais 5 centímetros de altura a todos
+
+
+
+# dplyr -----------------------------------------------------------------------
+
+# exercício 1
+# Há filmes com nota menor do que 5 e um orcamento maior que 1 milhão de dólares?
+
+# exercício 2
+# Crie uma tabela apenas com filmes do Spike Lee e as colunas titulo e ano
+# ordenada por ano.
+
+# exercício 3
+# Crie uma variável chamada lucro com o lucro em milhões de dólares.
+# Salve em um objeto chamado imdb_lucro.
+
+
+# exercício 4
+# Calcule a duração média e mediana dos filmes da base.
+
+
+# exercício 5
+# Calcule a duração média e mediana dos filmes por classificação indicativa.
+
+
+
+# ggplot2 ---------------------------------------------------------------------
+# Exercício 1
+# Faça um gráfico de barras da média da duração dos filmes de cada categoria
+
+
+# Exercício 2
+# Faça um gráfico de linhas da média da duração dos filmes de cada ano
+
+
+# Exercício 3
+# Faça um gráfico de linhas da média da duração dos filmes de cada ano.
+# Cada classificação indicativa deve ter sua própria linha e cor.
+
+
+# Exercício 4
+# Faça um gráfico de dispersão entre duração do filme e sua nota.
+
+
+# Exercício 5
+# Faça um gráfico de dispersão entre duração do filme e sua nota.
+# A cor deve refletir prejuízo ou lucro de cada filme
+
+
+
